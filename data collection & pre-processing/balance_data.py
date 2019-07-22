da23f04943796399335c96fadfbcf579c408cf29
mehul <mehul@officebrain.com>
2019-07-22 22:25:27 +0530

    First commit

--- data collection & pre-processing/balance_data.py
+++
@@ -1,7 +0,0 @@
-import os
-import random
-
-print(len(os.listdir('/home/software/mehul/cartisan_ikea/lamp/train_data')))
-
-while len(os.listdir('/home/software/mehul/cartisan_ikea/lamp/train_data')) > 500:
-	os.remove('/home/software/mehul/cartisan_ikea/lamp/train_data/'+ os.listdir('/home/software/mehul/cartisan_ikea/lamp/train_data/')[random.randint(0,500)])
